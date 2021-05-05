ea13b63b984cff93e918ae51010dc4536079d6f2
Remi <rwisni736@gmail.com>
2021-05-05 15:24:42 -0500

    vsl update

--- app/src/App.js
+++ app/src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import Header from './Components/Header.jsx';
 import VolatilityLong from './Components/VolatilityLong.jsx';
 
-
 function App() {
   return (
     <div className="App">
       <Header/>
       <VolatilityLong/>
+
     </div>
   );
 }
--- app/src/Components/VolatilityLong.jsx
+++ app/src/Components/VolatilityLong.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import UserInputVSL from './UserInputVSL.jsx';
 
 function VolatilityLong(){
     return (
@@ -7,9 +8,14 @@ function VolatilityLong(){
                 <h1>
                     Volatility Stop Loss Long
                 </h1>
+                
+                
             </div>
+            <UserInputVSL/>
         </div>
     )
 }
 
 export default VolatilityLong;
+
+{/* we will have 2 components, the input, and the output */} 
