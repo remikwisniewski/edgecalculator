8dbdfaa0ca89c510f551301d903b758330eea140
Remi <rwisni736@gmail.com>
2021-05-10 09:34:36 -0500

    added calculation functions

--- app/src/Components/ResultsVSL.jsx
+++ app/src/Components/ResultsVSL.jsx
@@ -10,24 +10,35 @@ import UserInputVSL from './UserInputVSL';
 Functions Calculating values
 */
 
+function SL1ATR(lowPrice, ATR){
+    return lowPrice - ATR;
+}
+
+function NumShares(actualPrice, lowPrice, ATR, maxSL){
+    var sl1 = lowPrice - ATR;
+    var num = actualPrice - sl1;
+    return maxSL / num;
+
+}
+
+function Forexlots(actualPrice, lowPrice, ATR, maxSL){
+    var sl1 = lowPrice - ATR;
+    var num = actualPrice - sl1;
+    var num2 = maxSL / num;
+    return num2 / 100000; 
+}
+
 
 class ResultsVSL extends React.Component{
     /* gets user input */
     
-    constructor(props){
-        super(props);
-        
-        this.state = {
-            actualPrice: '' 
-        }
-    }
 
     render() {
     return (
         <div>
             <form class = "resultForm">
                 <div class = "resultFormText">
-                    <p> StopLoss 2 ATR: </p> 
+                    <p> StopLoss 1 ATR: </p> 
                     <p> Number of Shares: </p>
                     <p> Forex Lots: </p>
                 </div>
